99b6e0d18cae926b04c5400cb207b0cc04641543
Alper <alper@Alpers-MBP.local>
2020-08-22 23:09:19 +0200

    example unit test fixed

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/modules/test/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
+    const msg = "HI THERE SWEETHEART! HOW ARE YOU?";
+    const wrapper = shallowMount(HelloWorld);
     expect(wrapper.text()).toMatch(msg);
   });
 });
